b7756017b603a97e13fe4eaab108e4cff329cc7a
Sahil Bhatt <sahilb1017@gmail.com>
2022-07-06 07:19:54 -0600

    distance calculation added

--- dataRecalculation.py
+++ dataRecalculation.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pandas import DataFrame as df
 import glob
 import numpy as np
+from haversine import haversine, Direction
 
 
 #START OF DEF
@@ -12,12 +13,25 @@ def newTimeStamp(df):
 
     for i in range(len(times)):
         times[i] = (times[i] - ref) / 1000
+
+    return times
 #END OF DEF
 
 
 #START OF DEF
-def calculateDistances(df):
-    x=2
+def calculateDistances(frame):
+    array = frame.to_numpy()
+    total = 0
+    distances = [0]
+
+    for i in range(len(array) - 1):
+        currPoint = (array[i][0], array[i][1])
+        nextPoint = (array[i+1][0], array[i+1][1])
+        currentDistance = haversine(currPoint, nextPoint)
+        total = total + currentDistance
+        distances.append(total)
+
+    return distances
 #END OF DEF
 
 
@@ -31,6 +45,7 @@ def main():
         distances = calculateDistances(df)
 
         df = df.assign(timeStamp = times)
+        df['Distance'] = distances
         path = file.replace("FilteredData", "FilteredDataNewTime")
         df.to_csv(path, index=False)
 
